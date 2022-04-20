bf7f1d0da2043c262267d590bce26a7b663c4375
Timur <timur@MacBook-Pro-Timur.local>
2022-04-20 15:02:23 +0500

    make it plazma

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const whitelist = ["https://plazma-company.firebaseapp.com/"]
+const whitelist = ["https://plazma-company.firebaseapp.com"]
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || whitelist.indexOf(origin) !== -1) {
